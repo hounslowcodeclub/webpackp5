c7ddab68c867b881997e45e2218031513b8937f3
hounslowcodeclub <hounslowcodeclub@gmail.com>
2018-05-25 13:18:46 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import 'p5'
 window.setup = function () {
-  console.log("Setup")
+  createCanvas(640, 480);
 }
 window.draw = function () {
-  ellipse(50, 50, 80, 80);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
 
